ff482c34780ab1ae3f1440df95be3801825aa89f
Md Mushtahid Salam <mushtahid@gmail.com>
2020-09-18 22:10:22 +0100

    Finished Code Shrinking 1
    -imported operator, matplotlib

--- 2. Code Shrinking 1.py
+++ 2. Code Shrinking 1.py
@@ -5,88 +5,92 @@ Created on Tue Sep 15 22:13:42 2020
 @author: Mushtahid
 """
 # Import modules
-import random
+import random # To generate a random number to decide how to move agents
+import operator # To get the larger x value of the agents
+import matplotlib.pyplot # To plot a scatter graph of the agents
 
 # Create agent list
 agents = []
 
-# Make y and x coordinate variables of agent0 randomly within a 100X100 grid
-y0 = random.randint(0,99)
-x0 = random.randint(0,99)
-
-# Append the agent list to add agent0
-agents.append([y0, x0])
+# Append the agent list to add agent0.
+# y0 and x0 replaced by random integers to make it more efficient and cleaner
+agents.append([random.randint(0,99), random.randint(0,99)])
 print(agents)
 
 # Check the initial coordinates of agent0
-print("Initial position of agent0:", "y0:", y0, ", " "x0:", x0) 
+print("Initial position of agent0:", "y0:", agents[0][0], ", " "x0:", agents[0][1]) 
 
 ''' Randomly move agent0 by 1 step based on the random value being less 
 or greater than 0.5'''
 if random.random() < 0.5:
-    y0 += 1
+    agents[0][0] += 1
 else:
-    y0 -= 1
+    agents[0][0] -= 1
 
 if random.random() < 0.5:
-    x0 += 1
+    agents[0][1] += 1
 else:
-    x0 -= 1
-    
-# Check the new position of agent0 after moving 1 step
-print("New position of agent0:", "y0:", y0, ", " "x0:", x0)
+    agents[0][1] -= 1
+print("New position of agent0:", "y0:", agents[0][0], ", " "x0:", agents[0][1])
     
 # Move agent0 again by 1 step
 if random.random() < 0.5:
-    y0 += 1
+    agents[0][0] += 1
 else:
-    y0 -= 1
+    agents[0][0] -= 1
 
 if random.random() < 0.5:
-    x0 += 1
+    agents[0][1] += 1
 else:
-    x0 -= 1
-
-# To check the 2nd new position of agent0 after moving 2 steps
-print("2nd New position of agent0:", "y0:", y0, ", " "x0:", x0)
+    agents[0][1] -= 1
+print("2nd New position of agent0:", "y0:", agents[0][0], ", " "x0:", agents[0][1])
     
-# Make y and x coordinate variables of agent1 randomly within a 100X100 grid
-y1 = random.randint(0,99)
-x1 = random.randint(0,99)
-
-# To check the initial coordinates of agent0
-print("Initial position of agent1:", "y1:", y1, ", " "x1:", x1) 
+# Add agent1 randomly within a 100X100 grid
+agents.append([random.randint(0,99), random.randint(0,99)])
+print("2nd position of agent0 and starting position of agent1:", agents)
+# Check the initial coordinates of agent1
+print("Initial position of agent1:", "y1:", agents[1][0], ", " "x1:", agents[1][1]) 
 
 '''Randomly move agent1 by 1 step based on the random value 
 being less or greater than 0.5'''
 if random.random() < 0.5:
-    y1 += 1
+    agents[1][0] += 1
 else:
-    y1 -= 1
+    agents[1][0] -= 1
 
 if random.random() < 0.5:
-    x1 += 1
+    agents[1][1] += 1
 else:
-    x1 -= 1
-    
-# Check the new position of agent1 after moving 1 step
-print("New position of agent1:", "y1:", y1, ", " "x1:", x1)
+    agents[1][1] -= 1 
+print("New position of agent1:", "y1:", agents[1][0], ", " "x1:", agents[1][1])
     
 # Move agent1 again by 1 step
 if random.random() < 0.5:
-    y1 += 1
+    agents[1][0] += 1
 else:
-    y1 -= 1
+    agents[1][0] -= 1
 
 if random.random() < 0.5:
-    x1 += 1
+    agents[1][1] += 1
 else:
-    x1 -= 1
-
+    agents[1][1] -= 1
 # Check the 2nd new position of agent0 after moving 2 steps
-print("2nd New position of agent1:", "y1:", y1, ", " "x1:", x1)
+print("2nd New position of agent1:", "y1:", agents[1][0], ", " "x1:", agents[1][1])
 
 # Calculate and Print the distance between the agent 0 & 1
-distance = (((y0-y1)**2) + ((x0-x1)**2))**0.5
+distance = (((agents[0][0]-agents[1][0])**2) + ((agents[0][1]-agents[1][1])**2))**0.5
 print("Distance between agents 0 & 1:", distance)
+
+#The agent at the furthest east(largest x)
+print("Furthest east agent:", max(agents, key=operator.itemgetter(1)))
+
+#Plot agents in a scatter graph
+matplotlib.pyplot.ylim(0, 99)
+matplotlib.pyplot.xlim(0, 99)
+matplotlib.pyplot.scatter(agents[0][1],agents[0][0])
+matplotlib.pyplot.scatter(agents[1][1],agents[1][0])
+#Color the furthest east agent red
+matplotlib.pyplot.scatter(max(agents, key=operator.itemgetter(1))[1], max(agents, key=operator.itemgetter(1))[0], color='red')
+matplotlib.pyplot.show()
+
 
