e9b490174e26f355602a0b7f593e263b78821c16
Md Mushtahid Salam <mushtahid@gmail.com>
2020-09-18 23:27:10 +0100

    Used torus in the plot

--- Code_shrinking_2.py
+++ Code_shrinking_2.py
@@ -9,29 +9,26 @@ import random # To generate a random number to decide how to move agents
 import operator # To get the larger x value of the agents
 import matplotlib.pyplot # To plot a scatter graph of the agents
 
-
 # Set the number of agents
 num_of_agents = 10
 # Set number of iterations (no. of coordination changes) for agents
 num_of_iterations = 100
 
-
 # Create agent list
 agents = []
 
-
-# Append the agent list to add agents using for-loop.
-# y0 and x0 replaced by random integers to make it more efficient and cleaner
+# Append the agent list to add the set number of agents using for-loop.
+# y and x replaced by random integers to make it more efficient and cleaner
 for i in range(num_of_agents):
     agents.append([random.randint(0,99), random.randint(0,99)])
 print("Initial coordinates (y,x) of agents:", agents)
 
 '''Nested for-loops. num_of_agents is within num_of_iterations because all 
-agents will move 1 step at a time and then the next (with a total step set by
-num_of_iterations)''' 
+agents will move 1 step at a time and then the next until the total
+number of steps are completed)''' 
 # 1st for-loop for total number of steps
 for j in range(num_of_iterations):
-    ## 2nd For-loop to move all agents 1 step on random value being < or >=0.5 
+    ## 2nd for-loop to move all agents 1 step on random value being < or >=0.5 
     for i in range(num_of_agents):
         ### Move Y coordinate
         if random.random() < 0.5:
@@ -43,15 +40,11 @@ for j in range(num_of_iterations):
            agents[i][1] += 1
         else:
            agents[i][1] -= 1
-        ### Check if off edge and adjust.
-        if agents[i][0] < 0:
-            agents[i][0] = 99
-        if agents[i][1] < 0:
-            agents[i][1] = 99
-        if agents[i][0] > 99:
-            agents[i][0] = 0
-        if agents[i][1] > 99:
-            agents[i][1] = 0
+        ### Check if off edge and adjust using modulus operator (Torus)
+        if random.random() < 0.5:
+            agents[i][0] = (agents[i][0] + 1) % 100
+        else:
+            agents[i][0] = (agents[i][0] - 1) % 100
 # Print new coordinates of all agents 
 print("New coordinates (y,x) of agents:", agents)
     
