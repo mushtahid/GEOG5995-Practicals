a98d7cc5aee44d15e041c75c36cd11b52d4b65a2
Md Mushtahid Salam <mushtahid@gmail.com>
2020-09-19 00:57:53 +0100

    Update 4.Building_tools.py
    - Torus corrected. In the code shrinking 2 the torus adjustment codes for x coordinates were not given.
    - also distance half optimized

--- 4.Building_tools.py
+++ 4.Building_tools.py
@@ -9,6 +9,23 @@ import random # To generate a random number to decide how to move agents
 import operator # To get the larger x value of the agents
 import matplotlib.pyplot # To plot a scatter graph of the agents
 
+# Create function to calculate distance between two agents in agent list
+def distance_between(a, b):
+    """
+    Calculate and returns the distance between agents a and b.
+    ----------
+    a : TYPE
+        DESCRIPTION.
+    b : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    Distance between agents a and b.
+    """
+    return (((a[0]-b[0])**2) + ((a[1]-b[1])**2))**0.5
+
+
 # Set the number of agents
 num_of_agents = 10
 # Set number of iterations (no. of coordination changes) for agents
@@ -25,31 +42,30 @@ print("Initial coordinates (y,x) of agents:", agents)
 
 '''Nested for-loops to move the agents. num_of_agents is within 
 num_of_iterations because all agents will move 1 step at a time and then the 
-next until the total number of steps are completed)''' 
+next until the total number of steps are completed).
+Torus is used to adjust and prevent agents going off edge''' 
 # 1st for-loop for total number of steps
 for j in range(num_of_iterations):
     ## 2nd for-loop to move all agents 1 step on random value being < or >=0.5 
     for i in range(num_of_agents):
-        ### Move Y coordinate
-        if random.random() < 0.5:
-            agents[i][0] += 1
-        else:
-            agents[i][0] -= 1
-        ### Move X coordinate
-        if random.random() < 0.5:
-           agents[i][1] += 1
-        else:
-           agents[i][1] -= 1
-        ### Check if off edge and adjust using modulus operator (Torus)
+        ### Move and adjust Y coordinate
         if random.random() < 0.5:
             agents[i][0] = (agents[i][0] + 1) % 100
         else:
             agents[i][0] = (agents[i][0] - 1) % 100
+        ### Move and adjust X coordinate    
+        if random.random() < 0.5:
+            agents[i][1] = (agents[i][1] + 1) % 100
+        else:
+            agents[i][1] = (agents[i][1] - 1) % 100
 # Print new coordinates of all agents 
 print("New coordinates (y,x) of agents:", agents)
+
+# Calculate the distance between agents  
+distance = distance_between(agents[0], agents[1])
+print(distance)
     
-    
-# Calculate the distance between the agent 0 & 1
+
 # distance = (((agents[0][0]-agents[1][0])**2) + ((agents[0][1]-agents[1][1])**2))**0.5
 # print("Distance between agents 0 & 1:", distance)
 
