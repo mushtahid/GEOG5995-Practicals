7b612375f8f1391aba6560671124d454a2ca2a5a
Md Mushtahid Salam <mushtahid@gmail.com>
2020-09-18 20:33:22 +0100

    Update 1.abm.py
    1. Added another agent. So now it has two agents.
    2. Calculated the distance between agent 0 and agent 1

--- 1.abm.py
+++ 1.abm.py
@@ -18,7 +18,9 @@ x0 = 50
 print("Initial position of agent0:", "y0:", y0, ", " "x0:", x0) 
 
 
-# Randomly move ageon 0 by 1 based on the random value being less or greater than 0.5
+
+''' Randomly move agent0 by 1 step based on the random value being less 
+or greater than 0.5'''
 if random.random() < 0.5:
     y0 += 1
 else:
@@ -31,5 +33,62 @@ else:
     
 # To check the new position of agent0 after being altered based on random value
 print("New position of agent0:", "y0:", y0, ", " "x0:", x0)
+    
+# Move agent0 again by 1 step
+if random.random() < 0.5:
+    y0 += 1
+else:
+    y0 -= 1
 
+if random.random() < 0.5:
+    x0 += 1
+else:
+    x0 -= 1
+
+# To check the 2nd new position of agent0
+print("2nd New position of agent0:", "y0:", y0, ", " "x0:", x0)
+    
+# Make y and x coordinate variables of agent1
+y1 = 50
+x1 = 50
+
+# To check the initial coordinates of agent0
+print("Initial position of agent1:", "y1:", y1, ", " "x1:", x1) 
+
+
+
+'''Randomly move agent1 by 1 step based on the random value 
+being less or greater than 0.5'''
+if random.random() < 0.5:
+    y1 += 1
+else:
+    y1 -= 1
+
+if random.random() < 0.5:
+    x1 += 1
+else:
+    x1 -= 1
+    
+# To check the new position of agent1 after being altered based on random value
+print("New position of agent1:", "y1:", y1, ", " "x1:", x1)
+    
+# Move agent1 again by 1 step
+if random.random() < 0.5:
+    y1 += 1
+else:
+    y1 -= 1
+
+if random.random() < 0.5:
+    x1 += 1
+else:
+    x1 -= 1
+
+# To check the 2nd new position of agent0
+print("2nd New position of agent1:", "y1:", y1, ", " "x1:", x1)
+
+# Calculate the distance between the two agents
+distance = (((y0-y1)**2) + ((x0-x1)**2))**0.5
+
+# Print the distance between agents 0 & 1
+print("Distance between agents 0 & 1:", distance)
 
