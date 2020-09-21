88e275bef439acb5191ab16e0b79c8f73318f93a
Md Mushtahid Salam <mushtahid@gmail.com>
2020-09-21 21:44:46 +0100

    imported agentframework

--- Agents.py
+++ Agents.py
@@ -5,11 +5,19 @@ Created on Tue Sep 15 22:13:42 2020
 @author: Mushtahid
 """
 # Import modules
-import random # To generate a random number to decide how to move agents
 import operator # To get the larger x value of the agents
 import matplotlib.pyplot # To plot a scatter graph of the agents
 import time # To calculate the time taken to sections of codes
-random.seed(10)
+import agentframework
+
+# Set random seed to help with checking calculations and getting
+# same results
+
+a = agentframework.Agent()
+print(a.y, a.x)
+a.move()
+print(a.y, a.x)
+
 # Create function to calculate distance between two agents in agent list
 def distance_between(a, b):
     """
@@ -24,22 +32,21 @@ def distance_between(a, b):
     -------
     Distance between agents a and b.
     """
-    return (((a[0]-b[0])**2) + ((a[1]-b[1])**2))**0.5
+    return (((a.x-b.x)**2) + ((a.y-b.y)**2))**0.5
 
 
 # Set the number of agents
 num_of_agents = 10
-# Set number of iterations (no. of coordination changes) for agents
+# Set number of iterations (no. of steps) for agents
 num_of_iterations = 100
-
 # Create agent list
 agents = []
 
 # Make agents using for-loop.
 # y and x replaced by random integers to make it more efficient and cleaner
 for i in range(num_of_agents):
-    agents.append([random.randint(0,99), random.randint(0,99)])
-print("Initial coordinates (y,x) of agents:", agents)
+    agents.append(agentframework.Agent())
+#print("Initial coordinates (y,x) of agents:", agents)
 
 '''Nested for-loops to move the agents. num_of_agents is within 
 num_of_iterations because all agents will move 1 step at a time and then the 
@@ -49,18 +56,11 @@ Torus is used to adjust and prevent agents going off edge'''
 for j in range(num_of_iterations):
     ## 2nd for-loop to move all agents 1 step on random value being < or >=0.5 
     for i in range(num_of_agents):
-        ### Move and adjust Y coordinate
-        if (random.random() < 0.5):
-            agents[i][0] = (agents[i][0] + 1) % 100
-        else:
-            agents[i][0] = (agents[i][0] - 1) % 100
-        ### Move and adjust X coordinate    
-        if (random.random() < 0.5):
-            agents[i][1] = (agents[i][1] + 1) % 100
-        else:
-            agents[i][1] = (agents[i][1] - 1) % 100
+        agents[i].move()
+   
+
 # Print new coordinates of all agents 
-print("New coordinates (y,x) of agents:", agents)
+#print("New coordinates (y,x) of agents:", agents)
 
 # Timing the distance calculation
 start = time.process_time() # Start timing
@@ -87,15 +87,15 @@ end = time.process_time()
 print("Time to calculate the distances: " + str(end - start)) 
 
 #The agent at the furthest east(largest x)
-print("Furthest east agent:", max(agents, key=operator.itemgetter(1)))
+#print("Furthest east agent:", max(agents, key=operator.itemgetter(1)))
 
 # Plot agents in a scatter graph
 matplotlib.pyplot.ylim(0, 99)
 matplotlib.pyplot.xlim(0, 99)
 ## For-loop to plot all agents
 for i in range(num_of_agents):
-    matplotlib.pyplot.scatter(agents[i][1],agents[i][0])
+    matplotlib.pyplot.scatter(agents[i].x, agents[i].y)
 ##Color the furthest east agent red
-matplotlib.pyplot.scatter(max(agents, key=operator.itemgetter(1))[1], max(agents, key=operator.itemgetter(1))[0], color='red')
+#matplotlib.pyplot.scatter(max(agents, key=operator.itemgetter(1))[1], max(agents, key=operator.itemgetter(1))[0], color='red')
 matplotlib.pyplot.show()
 
---
+++ agentframework.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 21 08:37:16 2020
+
+@author: Mushtahid
+"""
+
+import random
+
+class Agent:
+    def __init__(self):
+        self.x = random.randint(0,99)
+        self.y = random.randint(0,99)
+        
+    def move(self):
+        if random.random() < 0.5:
+            self.y = (self.y + 1) % 100
+        else:
+            self.y = (self.y - 1) % 100
+
+        if random.random() < 0.5:
+            self.x = (self.x + 1) % 100
+        else:
+            self.x = (self.x - 1) % 100
+        
+    
